c852ea2165bebedf445e215ee4d6669372c240cf
kalyantalbothula <kalyan6166@gmail.com>
2025-06-30 16:14:10 +1000

    roboshop shell script

---
+++ catalogue.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MONGODB_HOST=mongodb.kalyanu.xyz
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "ERROR:: $2 ... $R  Failed $N"
+        exit 1
+    else 
+        echo -e " $2 ... $G Success $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then 
+    echo -e "$R ERROR: please run the script with root accuss $N"
+    exit 1
+else 
+    echo "you are root user"
+fi 
+
+dnf module disable nodejs -y &>> $LOGFILE
+
+VALIDATE $? "Disabling current NodeJS Version"
+
+dnf module enable nodejs:18 -y &>> $LOGFILE
+
+VALIDATE $? "Enabling NodeJS:18 Version"
+
+dnf install nodejs -y 
+
+VALIDATE $? "Installing NodeJS "
+
+useradd roboshop &>> $LOGFILE
+
+VALIDATE $? "creating Roboshop User"
+
+mkdir /app &>> $LOGFILE
+
+VALIDATE $? "Creating APP directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip 
+
+VALIDATE $? "Downloading Catalogue application "
+
+cd /app &>> $LOGFILE
+
+VALIDATE $? "Change Directory to Application"
+
+unzip /tmp/catalogue.zip &>> $LOGFILE
+
+VALIDATE $? "Unziping Catalogue"
+
+npm install &>> $LOGFILE
+
+VALIDATE $? "Installing Dependences"
+
+# use absolute, because catalogue.service exists there
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
+
+VALIDATE $? "Coping Catalogue service file"
+
+systemctl daemon-reload
+
+VALIDATE $? "Catalogue Demon reload"
+
+systemctl enable catalogue
+
+VALIDATE $? "Enable Catalogue"
+
+systemctl start catalogue
+
+VALIDATE $? "Start Catalogue"
+
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+
+VALIDATE $? "Copying mongodb repo"
+
+dnf install mongodb-org-shell -y
+
+VALIDATE $? " Installing Mongodb Client"
+
+mongo --host $MONGODB_HOST </app/schema/catalogue.js
+
+VALIDATE $? " Loading Catalogue data into Mongodb"
